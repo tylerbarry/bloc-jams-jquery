6a9a868c912abb16c10d2735708e86a6f885ce23
tylerbarry <53415989+tylerbarry@users.noreply.github.com>
2020-01-21 09:52:54 -0500

    Update helper.js

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,10 +1,8 @@
-class Helper {
-  playPauseAndUpdate(song){
-    const duration = player.getDuration();
+class Helper{
+  playPauseAndUpdate (song) {
     player.playPause(song);
-    $('#time-control .total-time').text(
-      player.prettyTime( duration )
-    );
+    const totalTime = player.prettyTime(player.currentlyPlaying.duration);
+    $('#time-control .total-time').text( totalTime );
   }
 }
 
